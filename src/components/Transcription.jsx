621818ada4b02a6b968d5e3749927dc6c322eb05
rhiannonlind <133162388+rhiannonlind@users.noreply.github.com>
2025-06-08 16:30:44 +0100

    Resolve merge conflict and complete rebase

---
+++ src/components/Transcription.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Transcription(props) {
+    const { textElement } = props
+
+  return (
+    <div>
+      <div>{textElement}</div>
+    </div>
+  )
+}
